a821ac57ea3432501dfb52878882980d39158717
grphine <grphine@gmail.com>
2023-07-05 20:07:12 +0100

    added message+code framework

---
+++ fa.py
@@ -0,0 +1,35 @@
+import csv
+
+# list of 'letters' by frequency
+frequency_list = list('etaoinshrdlcumwfgypbvkjxqz')
+# print(l)
+
+# dictionary of frequency map
+dictionary = {"1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0, "8": 0, "9": 0, "10": 0, "11": 0, "12": 0, "13": 0, "14": 0, "15": 0, "16": 0, "17": 0, "18": 0, "19": 0, "20": 0, "21": 0, "22": 0, "23": 0, "24": 0, "25": 0, "26": 0}
+
+# ts = ""
+# for k in range(27):
+#     ts2 = '"{}": 0, '
+#     ts = ts + ts2.format(k)
+# print(ts)    
+
+with open('messages.csv', newline='') as file:
+    reader = csv.reader(file, delimiter=',')
+    for row in reader:
+        for col in row:
+            dictionary[col] = dictionary[col] + 1
+    sorted_dictionary = sorted(dictionary)
+    # for row in reader:
+    #     for col in row:
+    #         index = sorted_dictionary.index(col)
+    #         print(frequency_list[index])
+
+with open('messages.csv', newline='') as file:
+    reader = csv.reader(file, delimiter=',')
+    for row in reader:
+        ps = ""
+        for col in row:
+            index = sorted_dictionary.index(col)
+            ps = ps + frequency_list[index]
+        print(ps)
+
