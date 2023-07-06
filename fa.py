9799c3971112f7cfa7cdc2ca1c958a9a91b886db
grphine <grphine@gmail.com>
2023-07-06 10:37:19 +0100

    improved frequency sorting

--- fa.py
+++ fa.py
@@ -1,7 +1,7 @@
 import csv
 
 # list of 'letters' by frequency
-frequency_list = list('etaoinshrdlcumwfgypbvkjxqz')
+frequency_list = list('.etaoinshrdlcumwfgypbvkjxqz')
 # print(l)
 
 # dictionary of frequency map
@@ -18,7 +18,8 @@ with open('messages.csv', newline='') as file:
     for row in reader:
         for col in row:
             dictionary[col] = dictionary[col] + 1
-    sorted_dictionary = sorted(dictionary)
+    # sorted_dictionary = sorted(dictionary)
+    sorted_dictionary = sorted(dictionary.items(), key=lambda x: x[1], reverse=True)
     # for row in reader:
     #     for col in row:
     #         index = sorted_dictionary.index(col)
@@ -28,8 +29,16 @@ with open('messages.csv', newline='') as file:
     reader = csv.reader(file, delimiter=',')
     for row in reader:
         ps = ""
+        print(frequency_list)
+        print(sorted_dictionary)
+        print(dictionary)
         for col in row:
-            index = sorted_dictionary.index(col)
+            for tup in sorted_dictionary:
+                if tup[0] == col:
+                    index = int(tup[0])
+                    # print(index)
             ps = ps + frequency_list[index]
+            # print(index)
+            # print(frequency_list[index])
         print(ps)
 
